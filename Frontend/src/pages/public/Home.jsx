2dc1cb6571ff524ec387ca7b7d05afd987b91926
noushinizar <noushinizar8129@gmail.com>
2026-08-04 15:58:47 +0530

    Fix import case for Stats component

--- Frontend/src/pages/public/Home.jsx
+++ Frontend/src/pages/public/Home.jsx
@@ -1,15 +1,12 @@
 import CTA from "../../components/landing/CTA";
 import FeaturedCourses from "../../components/landing/FeaturedCourses";
 import Hero from "../../components/landing/Hero";
-import Navbar from "../../components/landing/Navbar";
-import Stats from "../../components/landing/stats";
 import WhyChooseUs from "../../components/landing/WhyChooseUs";
 
 const Home = () => {
   return (
     <>
-    <Hero />
-    {/* <Stats/> */}
+    <Hero /> 
     <FeaturedCourses />
      <WhyChooseUs />
      <CTA/>
