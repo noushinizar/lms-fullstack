17ec78ab88c1570a04b3b065ab1c9c3fc9695915
noushinizar <noushinizar8129@gmail.com>
2026-08-17 15:04:15 +0530

    password visibility updated

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -218,13 +218,13 @@ function Login() {
                   className="w-full bg-transparent outline-none"
                 />
 
-                <Button 
+                <button 
                 type="button"
                 onClick={()=>setShowPassword(!showPassword)}
                 className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
                 >
                  {showPassword ? <FaEyeSlash/> : <FaEye/>}
-                </Button>
+                </button>
 
               </div>
 
