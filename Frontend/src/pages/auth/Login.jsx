7e4fb14ba672705910cd97b94a516e9abc29baa2
noushinizar <noushinizar8129@gmail.com>
2026-08-17 15:08:09 +0530

    password visibility updated

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -218,13 +218,16 @@ function Login() {
                   className="w-full bg-transparent outline-none"
                 />
 
-                <button 
+                <button
                 type="button"
-                onClick={()=>setShowPassword(!showPassword)}
-                className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
-                >
-                 {showPassword ? <FaEyeSlash/> : <FaEye/>}
-                </button>
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FaEyeSlash className="text-gray-600" />
+                ) : (
+                  <FaEye className="text-gray-600" />
+                )}
+              </button>
 
               </div>
 
--- Frontend/src/pages/auth/Register.jsx
+++ Frontend/src/pages/auth/Register.jsx
@@ -419,14 +419,19 @@ function Register() {
                   onChange={handleConfirmPasswordChange}
                   className="w-full bg-transparent outline-none"
                 />
-                   <button
-                 type="button"
-                 onClick={()=> setShowPassword(!showPassword)}
-                 className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
-               >
+                  
 
-               {showPassword ? <FaEyeSlash/> : <FaEye/>}
-               </button>
+               <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FaEyeSlash className="text-gray-600" />
+                ) : (
+                  <FaEye className="text-gray-600" />
+                )}
+              </button>
+               
               </div>
 
               {errors.confirmPassword && (
